a471b902db8a65ee2666c4fa8c37d044970d617a
Bogdan <bogdanskrip404@gmail.com>
2026-01-25 23:50:45 +0500

    Initial commit

---
+++ Study-System/src/main/java/com/example/Study/System/StudySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Study.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySystemApplication.class, args);
+	}
+
+}
